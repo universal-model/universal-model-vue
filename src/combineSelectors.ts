94f020804d3c27580243a68b4647907bb1d4b882
Petri Silen <petri.silen@silensoft.com>
2020-01-26 09:58:04 +0200

    Modified

---
+++ src/combineSelectors.ts
@@ -0,0 +1,15 @@
+import * as _ from 'lodash';
+import { Selectors, SelectorsBase, State } from './Store';
+
+export default function<T extends State, U extends SelectorsBase<T>>(
+  selectorsObjects: Array<Selectors<T, SelectorsBase<T>>>
+): Selectors<T, U> {
+  return _.mergeWith(
+    selectorsObjects[0],
+    ...selectorsObjects.slice(1),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (objValue: any, srcValue: any, key: string) => {
+      if (objValue !== undefined) throw new Error(`duplicate selector key: ${key}`);
+    }
+  );
+}
