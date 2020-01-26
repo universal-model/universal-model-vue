be6788ec714745c1fb14105e284696984bb5f958
Petri Silen <petri.silen@silensoft.com>
2020-01-26 10:00:45 +0200

    Modified

--- src/combineSelectors.ts
+++ src/combineSelectors.ts
@@ -9,7 +9,7 @@ export default function<T extends State, U extends SelectorsBase<T>>(
     ...selectorsObjects.slice(1),
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (objValue: any, srcValue: any, key: string) => {
-      if (objValue !== undefined) throw new Error(`duplicate selector key: ${key}`);
+      if (!_.isUndefined(objValue)) throw new Error(`duplicate selector key: ${key}`);
     }
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as createStore } from './createStore';
+export { default as combineSelectors } from './combineSelectors';
 export { default as Store } from './Store';
